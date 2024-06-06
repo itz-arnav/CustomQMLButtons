bcdd05c1f029331176c0e77a8b65d9c33e19acd2
Arnab Kumar Jha <arnab.jha@enpass.io>
2024-06-06 12:32:36 +0530

    Added app icon

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QIcon>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/images/logo.png"));
     QQmlApplicationEngine engine;
     const QUrl url(QStringLiteral("qrc:/CustomButton/Main.qml"));
     QObject::connect(
